420e4bbb3dd5cec0a70475bc6f9e634f8bc116e0
Chris Scott <chris@chrisdjscott.co.uk>
2012-05-19 07:16:06 +0100

    Bug in Makefile, added clean option to setup.py.

--- src/setup.py
+++ src/setup.py
@@ -18,18 +18,35 @@ import utilities
 ################################################################################
 def main():
     
-    pyrcc4 = utilities.checkForExe("pyrcc4")
-    
-    # on mac it is appended with python version
-    if not pyrcc4:
-        pyrcc4 = utilities.checkForExe("pyrcc4-%d.%d" % (sys.version_info[0], sys.version_info[1]))
-    
-    if not pyrcc4:
-        sys.exit("ERROR: COULD NOT LOCATE PYRCC4")
-    
-    command = "%s resources.qrc > resources.py" % (pyrcc4,)
-    print command
-    os.system(command)
+    if len(sys.argv) == 2 and sys.argv[1] == "clean":
+        command = "rm -f *.pyc"
+        print command
+        os.system(command)
+        
+        os.chdir("visclibs")
+        command = "make clean"
+        print command
+        os.system(command)
+        
+    else:
+        pyrcc4 = utilities.checkForExe("pyrcc4")
+        
+        # on mac it is appended with python version
+        if not pyrcc4:
+            pyrcc4 = utilities.checkForExe("pyrcc4-%d.%d" % (sys.version_info[0], sys.version_info[1]))
+        
+        if not pyrcc4:
+            sys.exit("ERROR: COULD NOT LOCATE PYRCC4")
+        
+        command = "%s resources.qrc > resources.py" % (pyrcc4,)
+        print command
+        os.system(command)
+        
+        # run Makefile
+        os.chdir("visclibs")
+        command = "make"
+        print command
+        os.system(command)
 
 
 ################################################################################
