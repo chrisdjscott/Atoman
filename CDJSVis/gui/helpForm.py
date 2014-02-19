a3118d2bfbd6333bb5e3907a4f0616d9450900d3
Chris Scott <chris@chrisdjscott.co.uk>
2014-02-19 15:33:38 +0000

    Sphinx html documentation include in resources file.
     - setup script builds sphinx doc and puts it in qrc file before building resources
     - help page uses qrc:// link to resources html files

--- CDJSVis/gui/helpForm.py
+++ CDJSVis/gui/helpForm.py
@@ -5,10 +5,12 @@ Help form
 @author: Chris Scott
 
 """
+import logging
+
 from PySide import QtGui, QtCore, QtWebKit
 
-
 from ..visutils.utilities import iconPath
+from .. import resources
 
 
 ################################################################################
@@ -95,37 +97,16 @@ class HelpFormSphinx(QtGui.QDialog):
         
         self.webView = QtWebKit.QWebView(self)
         
-        self.webView.load("/Volumes/Users_HD/Users/macdjs/git/CDJSVis/doc/_build/html/index.html")
+        logger = logging.getLogger(__name__)
+        logger.debug("Setting up help form")
+        
+        self.webView.load("qrc:///doc/index.html")
         self.webView.show()
         
         layout = QtGui.QVBoxLayout(self)
         row = QtGui.QHBoxLayout()
         row.addWidget(self.webView)
         layout.addLayout(row)
-        
-        # create splitter
-#         self.splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
-#         
-#         # create helper and set it up
-#         self.helpEngine = QtHelp.QHelpEngine("/Volumes/Users_HD/Users/macdjs/git/CDJSVis/doc/_build/qthelp/CDJSVis.qhc", self)
-#         self.helpEngine.setupData()
-#         
-#         # get the content widget and browser
-#         self.contentWidget = self.helpEngine.contentWidget()
-#         self.helpBrowser = HelpBrowser(self.splitter, self.helpEngine)
-#         
-#         # build widget
-#         self.splitter.insertWidget(0, self.contentWidget) 
-#         self.splitter.insertWidget(1, self.helpBrowser) 
-#         self.splitter.setStretchFactor(1, 1) 
-#         
-#         layout = QtGui.QVBoxLayout(self)
-#         row = QtGui.QHBoxLayout()
-#         row.addWidget(self.splitter)
-#         layout.addLayout(row)
-#         
-#         # connect the TOC to the help browser
-#         self.helpEngine.contentWidget().linkActivated.connect(self.helpBrowser.setSource)
         
         self.resize(900, 700)
     
@@ -136,26 +117,3 @@ class HelpFormSphinx(QtGui.QDialog):
         """
         self.parent.helpOpen = 0
         self.hide()
-
-################################################################################
-
-class HelpBrowser(QtGui.QTextBrowser):
-    """
-    Help browser
-    
-    """
-    def __init__(self, parent, helpEngine):
-        super(HelpBrowser, self).__init__(parent)
-        
-        self.helpEngine = helpEngine
-    
-    def loadResource(self, loadType, url):
-        """
-        Load resource
-        
-        """
-        if url.scheme() != "qthelp":
-            return super(HelpBrowser, self).loadResource(loadType, url)
-        
-        return self.helpEngine.fileData(url)
-
