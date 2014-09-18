17e61b7d5c1edc17a8527e6d54596e0e8f17e233
Chris Scott <chris@chrisdjscott.co.uk>
2014-09-18 16:09:14 +0100

    More tests for vectors C lib

--- CDJSVis/tests/test_vectors.py
+++ CDJSVis/tests/test_vectors.py
@@ -34,3 +34,69 @@ class TestVectors(unittest.TestCase):
         vect = np.random.rand(100)
         mag = vectors.magnitude(vect)
         self.assertAlmostEqual(mag, np.linalg.norm(vect))
+    
+    def test_separationMagnitude(self):
+        """
+        Vector separation magnitude
+        
+        """
+        cellDims = np.asarray([100,0,0,0,100,0,0,0,100], dtype=np.float64)
+        
+        p1 = np.asarray([1.0, 1.0, 1.0], dtype=np.float64)
+        p2 = np.asarray([11.0, 5.0, 0.0], dtype=np.float64)
+        
+        sepMag = vectors.separationMagnitude(p1, p2, cellDims, np.ones(3, np.int32))
+        
+        self.assertAlmostEqual(sepMag, 10.81665383)
+    
+    def test_separationMagnitudePBC(self):
+        """
+        Vector separation magnitude PBC
+        
+        """
+        cellDims = np.asarray([100,0,0,0,100,0,0,0,100], dtype=np.float64)
+        
+        p1 = np.asarray([1.0, 1.0, 1.0], dtype=np.float64)
+        p2 = np.asarray([99, 99, 99], dtype=np.float64)
+        
+        sepMag = vectors.separationMagnitude(p1, p2, cellDims, np.ones(3, np.int32))
+        
+        self.assertAlmostEqual(sepMag, 3.464101615)
+    
+    def test_separationVector(self):
+        """
+        Vector separation vector
+        
+        """
+        cellDims = np.asarray([100,0,0,0,100,0,0,0,100], dtype=np.float64)
+        
+        p1 = np.asarray([1.0, 1.0, 1.0], dtype=np.float64)
+        p2 = np.asarray([11.0, 5.0, 0.0], dtype=np.float64)
+        
+        sepVec = np.empty(3, np.float64)
+        
+        status = vectors.separationVector(sepVec, p1, p2, cellDims, np.ones(3, np.int32))
+        
+        self.assertEqual(status, 0)
+        self.assertEqual(sepVec[0], 10)
+        self.assertEqual(sepVec[1], 4)
+        self.assertEqual(sepVec[2], -1)
+    
+    def test_separationVectorPBC(self):
+        """
+        Vector separation vector PBC
+        
+        """
+        cellDims = np.asarray([100,0,0,0,100,0,0,0,100], dtype=np.float64)
+        
+        p1 = np.asarray([1.0, 1.0, 1.0], dtype=np.float64)
+        p2 = np.asarray([99, 99, 99], dtype=np.float64)
+        
+        sepVec = np.empty(3, np.float64)
+        
+        status = vectors.separationVector(sepVec, p1, p2, cellDims, np.ones(3, np.int32))
+        
+        self.assertEqual(status, 0)
+        self.assertEqual(sepVec[0], -2)
+        self.assertEqual(sepVec[1], -2)
+        self.assertEqual(sepVec[2], -2)
