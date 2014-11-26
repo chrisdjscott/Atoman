19957f02a54cdbf2cf714bc22fa9686d3565d357
Chris Scott <C.D.J.Scott@lboro.ac.uk>
2014-11-26 14:04:58 +0000

    BCC lattice generator written in C to improve performance.

--- CDJSVis/lattice_gen/lattice_gen_bcc.py
+++ CDJSVis/lattice_gen/lattice_gen_bcc.py
@@ -7,8 +7,10 @@ Generate BCC lattice
 """
 import logging
 
-from ..state.lattice import Lattice
+import numpy as np
 
+from ..state.lattice import Lattice
+from . import _lattice_gen_bcc
 
 ################################################################################
 
@@ -57,64 +59,39 @@ class BCCLatticeGenerator(object):
         logger = logging.getLogger(__name__)
         logger.info("Generating BCC lattice")
         
-        # lattice constants
+        # numpy arrays
+        numCells = np.asarray(args.NCells, dtype=np.int32)
+        pbc = np.asarray([args.pbcx, args.pbcy, args.pbcz], dtype=np.int32)
+        
+        # lattice constant
         a0 = args.a0
-        a1 = a0 / 2.0
-        
-        # define primitive cell (4 atoms)
-        # corner atom is Ga
-        UC_rx = [0.0, a1]
-        UC_ry = [0.0, a1]
-        UC_rz = [0.0, a1]
-        
-        # handle PBCs
-        if args.pbcx:
-            iStop = args.NCells[0]
-        else:
-            iStop = args.NCells[0] + 1
-        
-        if args.pbcy:
-            jStop = args.NCells[1]
-        else:
-            jStop = args.NCells[1] + 1
-        
-        if args.pbcz:
-            kStop = args.NCells[2]
-        else:
-            kStop = args.NCells[2] + 1
         
         # lattice dimensions
-        dims = [a0*args.NCells[0], a0*args.NCells[1], a0*args.NCells[2]]
+        dims = [a0 * numCells[0], a0 * numCells[1], a0 * numCells[2]]
+        
+        # generate lattice data
+        NAtoms, specie, pos, charge = _lattice_gen_bcc.generateBCCLattice(numCells, pbc, a0)
         
         # lattice structure
         lattice = Lattice()
         
+        # set up correctly for this number of atoms
+        lattice.reset(NAtoms)
+        
         # set dimensions
         lattice.setDims(dims)
         
-        # generate lattice
-        count = 0
-        for i in xrange(iStop):
-            for j in xrange(jStop):
-                for k in xrange(kStop):
-                    for l in xrange(2):
-                        # position of new atom
-                        rx_tmp = UC_rx[l] + i * a0
-                        ry_tmp = UC_ry[l] + j * a0
-                        rz_tmp = UC_rz[l] + k * a0
-                        
-                        # skip if outside lattice (ie when making extra cell to get surface for non-periodic boundaries)
-                        if (rx_tmp > dims[0]+0.0001) or (ry_tmp > dims[1]+0.0001) or (rz_tmp > dims[2]+0.0001):
-                            continue
-                        
-                        # add to lattice structure
-                        lattice.addAtom(args.sym, (rx_tmp, ry_tmp, rz_tmp), 0.0)
-                        
-                        count += 1
+        # set data
+        lattice.addSpecie(args.sym, count=NAtoms)
+        lattice.specie = specie
+        lattice.pos = pos
+        lattice.charge = charge
+        lattice.NAtoms = NAtoms
         
-        NAtoms = count
-        
-        assert NAtoms == lattice.NAtoms
+        # min/max pos
+        for i in xrange(3):
+            lattice.minPos[i] = np.min(lattice.pos[i::3])
+            lattice.maxPos[i] = np.max(lattice.pos[i::3])
         
         logger.info("  Number of atoms: %d", NAtoms)
         logger.info("  Dimensions: %s", str(dims))
---
+++ CDJSVis/lattice_gen/setup.py
@@ -0,0 +1,21 @@
+
+import os
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    
+    # path to header files
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    incdir = os.path.abspath(os.path.join(cwd, "..", "visclibs"))
+    
+    # config
+    config = Configuration("lattice_gen", parent_package, top_path)
+    
+    config.add_extension("_lattice_gen_bcc", 
+                         ["_lattice_gen_bcc.c", "../visclibs/array_utils.c"],
+                         include_dirs=[incdir])
+    
+    return config
+
+if __name__ == "__main__":
+    print "This is the wrong setup.py to run"
