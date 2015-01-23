6087211a213aeaadc0d5bb676297134d29604869
Chris Scott <C.D.J.Scott@lboro.ac.uk>
2015-01-23 15:58:33 +0000

    Update year

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CDJSVis'
-copyright = u'2014, Chris Scott'
+copyright = u'2015, Chris Scott'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
