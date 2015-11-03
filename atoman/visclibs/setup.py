3734617142f699e672c54770e9af567dad81692a
Chris Scott <C.D.J.Scott@lboro.ac.uk>
2015-11-03 06:43:18 +0000

    Add empty files for adding boxeslib tests to.

--- atoman/visclibs/setup.py
+++ atoman/visclibs/setup.py
@@ -1,14 +1,24 @@
 
+import os
+
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     
     config = Configuration("visclibs", parent_package, top_path)
+
+    # add libraries
     config.add_library("boxeslib", ["boxeslib.c"], depends=["boxeslib.h"])
     config.add_library("utilities", ["utilities.c"], depends=["utilities.h"])
     config.add_library("neb_list", ["neb_list.c", "boxeslib.c", "utilities.c"],
                        depends=["neb_list.h", "boxeslib.h", "utilities.h"])
     config.add_library("array_utils", ["array_utils.c"], depends=["array_utils.h"])
+
+    # add extensions (for testing)
+    config.add_extension("tests._test_boxeslib",
+                         ["tests/test_boxeslib.c"],
+                         libraries=["boxeslib"],
+                         depends=[os.path.join("..", "boxeslib.h")])
     
     return config
 
