0e8304049b8c1282ef0c0222b65c43bfb3e5a9a0
Chris Scott <chris@chrisdjscott.co.uk>
2018-06-07 20:08:50 +1200

    Resolving conda dependency errors

--- scripts/install_conda.sh
+++ scripts/install_conda.sh
@@ -29,7 +29,6 @@ PYVER=3
 VTKVER=8
 CONDENV=atoman
 WITH_GCC=0
-QTVER=4.8.7
 PYSIDEVER=1.2.4
 
 # parse args
@@ -141,7 +140,7 @@ case ${VTKVER} in
     VTKVER=7.1.1
     ;;
     8)
-    VTKVER=8.1.1
+    VTKVER=8.1.0
     ;;
     *)
     echo "Error: VTK version must be '7' or '8' ('8' is default)"
@@ -191,7 +190,7 @@ conda update --yes --quiet conda
 # create conda environment
 echo Creating conda environment: \"${CONDENV}\"...
 conda create -y -q -n ${CONDENV} python=${PYVER} numpy scipy matplotlib pillow pip nose setuptools sphinx \
-        sphinx_rtd_theme paramiko vtk=${VTKVER} qt=${QTVER} pyside=${PYSIDEVER}
+        sphinx_rtd_theme paramiko vtk=${VTKVER} pyside=${PYSIDEVER}
 
 # install python.app on Mac, required for qt_menu.nib in pyinstaller builds
 if [[ "${CONDOS}" == "MacOSX" ]]; then
